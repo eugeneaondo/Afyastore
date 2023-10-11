0aac4f8a085ccab0ae68345ff103aac6bedf6448
eugeneaondo <eugeneaondo11@gmail.com>
2023-10-11 17:42:47 +0300

    Erros and changes

--- payments/apps.py
+++ payments/apps.py
@@ -1,6 +1,11 @@
 from django.apps import AppConfig
 
 
+from django.contrib import admin
+
+
+
+
 class PaymentsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'payments'
