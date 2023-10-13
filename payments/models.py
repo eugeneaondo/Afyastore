e2d23afc631fd2b0248e9a9087ff35a4149bd20a
eugeneaondo <eugeneaondo11@gmail.com>
2023-10-13 13:07:44 +0300

    error cleaning

--- payments/models.py
+++ payments/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 
 class PaymentMode(models.Model):
-    mode = models.CharField(max_length=50)
+    mode = models.CharField(max_length=50, unique=True)
     description = models.CharField(max_length=100)
 
 
