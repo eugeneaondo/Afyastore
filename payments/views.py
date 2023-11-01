1c6798f1f9c3e8d4dcbfc3697226f4f2b9df45b0
eugeneaondo <eugeneaondo11@gmail.com>
2023-11-01 16:48:15 +0300

    payments_changes

--- payments/views.py
+++ payments/views.py
@@ -1,16 +1,20 @@
-from django.shortcuts import render,HttpResponse
+from django.shortcuts import render,HttpResponse,get_object_or_404
 
 from .models import Payment
 
 # Create your views here.
 
 def all_payments(request):
-    Payments = Payment.objects.all()
-    return HttpResponse('Here are all your payments')
+    payments = Payment.objects.order_by('trans_date')
+    context = { "payments" : payments}
+    return render(request , "payments/payments_all.html" , context)
+
+
 
 
 def payment_details(request, payment_id):
-    response = "the payment details are %s"
-    return HttpResponse(response % payment_id)
+    payment = get_object_or_404(Payment, pk=payment_id)
+    return render(request, "payments/payment_detail.html" , {"payment" : payment})
+
 
 
